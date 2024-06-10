d284d714a387e391a5e073feee6fdbcfc5f847d4
Ualace <ualacehenrique@hotmail.com>
2024-06-10 18:14:28 -0300

    feat: implementa o analisador sintatico

--- src/cosmo/Parser.java
+++ src/cosmo/Parser.java
@@ -6,16 +6,20 @@ public class Parser {
     private List<Token> tokens;
     private Token current_token;
     private int current_pos = 0;
-    private int error_count = 0;
+    public int error_count = 0;
 
     public Parser(List<Token> tokens) {
         this.tokens = tokens;
         current_token = tokens.get(0);
     }
 
+    public int get_error_count() {
+        return error_count;
+    }
+
     private void report_error(Token token, String expected) {
         System.err.println(
-            current_token.line + ":" + current_token.column + ": expected `" + expected + "`, got `" + Token.spellings[current_token.kind] + "`"
+            current_token.line + ":" + current_token.column + ": error: expected `" + expected + "`, got `" + current_token.spelling + "`"
         );
         error_count++;
     }
@@ -38,6 +42,34 @@ public class Parser {
         }
     }
 
+    // <op-ad> ::= + | - | or
+    private boolean is_operator_ad() {
+        if (current_token.kind == Token.PLUS || current_token.kind == Token.MINUS || current_token.kind == Token.OR) {
+            return true;
+        }
+
+        return false;
+    }
+
+    // <op-mul> ::= * | / | and
+    private boolean is_operator_mul() {
+        if (current_token.kind == Token.TIMES || current_token.kind == Token.DIVIDE || current_token.kind == Token.AND) {
+            return true;
+        }
+
+        return false;
+    }
+
+    // <op-rel> ::= < | > | =
+    private boolean is_operator_rel() {
+        if (current_token.kind == Token.LESS || current_token.kind == Token.GREATER || current_token.kind == Token.EQUALS) {
+            return true;
+        }
+
+        return false;
+    }
+
+    // <program> ::= program <id> ; <body> .
     private void parse_program() {
         expect_identifier();
         if (current_token.spelling.equals("program")) {
@@ -51,11 +83,17 @@ public class Parser {
         accept(Token.DOT);
     }
 
+    // <body> ::= (<declaration>;)* <compound-statement>
     private void parse_body() {
-        parse_declaration();
+        while (current_token.kind == Token.IDENTIFIER && !current_token.spelling.equals("begin")) {
+            parse_declaration();
+            accept(Token.SEMICOLON);
+        }
+
         parse_compound_statement();
     }
 
+    // <declaration> ::= var <id> : <type>
     private void parse_declaration() {
         expect_identifier();
         if (current_token.spelling.equals("var")) {
@@ -68,6 +106,7 @@ public class Parser {
         parse_type();
     }
 
+    // <type> ::= integer | boolean
     private void parse_type() {
         expect_identifier();
         if (current_token.spelling.equals("integer")) {
@@ -79,6 +118,7 @@ public class Parser {
         }
     }
 
+    // <compound-statement> ::= begin <statement-list> end
     private void parse_compound_statement() {
         expect_identifier();
         if (current_token.spelling.equals("begin")) {
@@ -95,11 +135,133 @@ public class Parser {
         }
     }
 
-    // TODO:
-    // <statement_list> ::= <statement>; | <statement_list> <statement>; | empty
-    // ==
     // <statement_list> ::= (<statement>;)*
     private void parse_statement_list() {
+        while (current_token.kind == Token.IDENTIFIER) {
+            if (current_token.spelling.equals("end"))
+                break;
+
+            parse_statement();
+            accept(Token.SEMICOLON);
+        }
+    }
+
+    // <statement> ::= <assignment> | <conditional> | <iteration> | <compound-statement>
+    private void parse_statement() {
+        expect_identifier();
+        if (current_token.spelling.equals("if")) {
+            parse_conditional();
+        } else if (current_token.spelling.equals("while")) {
+            parse_iteration();
+        } else if (current_token.spelling.equals("begin")) {
+            parse_compound_statement();
+        } else {
+            parse_assignment();
+        }
+    }
+
+    // <conditional> ::= if <expression> then <statement> (else <statement> | ε)
+    private void parse_conditional() {
+        expect_identifier();
+        if (current_token.spelling.equals("if")) {
+            accept_it();
+        } else {
+            report_error(current_token, "if");
+        }
+        parse_expression();
+
+        expect_identifier();
+        if (current_token.spelling.equals("then")) {
+            accept_it();
+        } else {
+            report_error(current_token, "then");
+        }
+        parse_statement();
+
+        if (current_token.spelling.equals("else")) {
+            accept_it();
+            parse_statement();
+        }
+    }
+
+    // <expression> ::=
+    // <factor> (<op-mul> <factor>)* (<op-ad> <factor> (<op-mul> <factor>)*)* (<op-rel>  <factor> (<op-mul> <factor>)* (<op-ad> <factor> (<op-mul> <factor>)*)*)*
+    private void parse_expression() {
+        parse_factor();
+
+        while (is_operator_mul()) {
+            accept_it();
+            parse_factor();
+        }
+
+        while (is_operator_ad()) {
+            accept_it();
+            parse_factor();
+            while (is_operator_mul()) {
+                accept_it();
+                parse_factor();
+            }
+        }
+
+        while (is_operator_rel()) {
+            accept_it();
+            parse_factor();
+
+            while (is_operator_mul()) {
+                accept_it();
+                parse_factor();
+            }
+
+            while (is_operator_ad()) {
+                accept_it();
+                parse_factor();
+                while (is_operator_mul()) {
+                    accept_it();
+                    parse_factor();
+                }
+            }
+        }
+    }
+
+    // <factor> ::= <id> | <literal> | ( <expression> )
+    private void parse_factor() {
+        if (current_token.kind == Token.IDENTIFIER) {
+            accept_it();
+        } else if (current_token.kind == Token.INT_LITERAL || current_token.kind == Token.BOOL_LITERAL) {
+            accept_it();
+        } else if (current_token.kind == Token.LEFT_PAREN) {
+            accept_it();
+            parse_expression();
+            accept(Token.RIGHT_PAREN);
+        } else {
+            report_error(current_token, "<id>, <literal> or (");
+        }
+    }
+
+    // <iteration> ::= while <expression> do <statement>
+    private void parse_iteration() {
+        expect_identifier();
+        if (current_token.spelling.equals("while")) {
+            accept_it();
+        } else {
+            report_error(current_token, "while");
+        }
+        parse_expression();
+
+        expect_identifier();
+        if (current_token.spelling.equals("do")) {
+            accept_it();
+        } else {
+            report_error(current_token, "do");
+        }
+        parse_statement();
+    }
+
+    // <assignment> := <id> := <expression>
+    private void parse_assignment() {
+        accept(Token.IDENTIFIER);
+        accept(Token.ASSIGN);
+        parse_expression();
     }
 
     public void parse() {
@@ -107,10 +269,6 @@ public class Parser {
 
         if (current_token.kind != Token.EOF) {
             report_error(current_token, Token.spellings[Token.EOF]);
-        }
-
-        if (error_count != 0) {
-            System.exit(1);
         }
     }
 }
